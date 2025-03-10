867aeafb5581397496a99861e0c212426969120b
Alvin <lewiscreated@gmail.com>
2025-03-10 13:06:16 -0400

    changed website that we get request from to target a table that isn't dynamic.  table object created.

--- scratch_individual_page.py
+++ scratch_individual_page.py
@@ -1,8 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-r = requests.get('https://www.wnba.com/player/1628932')
+r = requests.get('https://www.unrivaled.basketball/stats/player')
+
 soup = BeautifulSoup(r.text, 'html.parser')
-player_name = soup.find(class_='Player_Player__info--primary__tQjoQ').find_all('h1')[0].find('span')
 
-print(r.text)
+# player_name = soup.find(class_='Player_Player__info--primary__tQjoQ').find_all('h1')[0].find('span')
+
+table = soup.find_all('table', class_="w-100 font-13")[0]
+
+table_str = str(table)
+
+table_panda = pd.read_html(table_str)
+
+
+
+print(table_panda)
