5eaa35d02c1d7e40a8848748b95f97db466e7bcf
Alvin <lewiscreated@gmail.com>
2025-03-10 21:13:30 -0400

    made changes to pd.DataFrame object.  Changed data source website. Modified offensive_leaders.py file to reflect new changes.

--- scratch_individual_page.py
+++ scratch_individual_page.py
@@ -1,18 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from io import StringIO
+
+import sqlite3
+from datetime import date
+conn = sqlite3.connect('unrivaled_stats.db')
+#c = conn.cursor()
 
 r = requests.get('https://www.unrivaled.basketball/stats/player')
-
 soup = BeautifulSoup(r.text, 'html.parser')
-
 # player_name = soup.find(class_='Player_Player__info--primary__tQjoQ').find_all('h1')[0].find('span')
-
+stat_name = soup.find(class_='p-4 flex-row-to-col-xs justify-between w-100 row-8').find('h2')
 table = soup.find_all('table', class_="w-100 font-13")[0]
-
 table_str = str(table)
+table_panda = pd.read_html(StringIO(table_str))[0]
 
-table_panda = pd.read_html(table_str)
+cols = table_panda.columns.tolist()
+cols.insert(0, 'Date Scrapped')
+cols.insert(1, 'Stats')
+table_panda['Stats'] = stat_name.text
+table_panda['Date Scrapped'] = date.today()
+table_panda = table_panda[cols]
+#table_panda.to_sql('unrivaled_stats', conn, if_exists='append', index=False)
 
 
 
